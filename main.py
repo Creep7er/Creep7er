d72e015e05ef305ddfc8545055a9df72fa90c229
Creep7er <glebmayorshin@gmail.com>
2024-01-13 16:31:40 +0500

    fix

--- main.py
+++ main.py
@@ -161,7 +161,7 @@ def main():
 <picture>
     <source media="(prefers-color-scheme: dark)" srcset="{image.url}">
     <source media="(prefers-color-scheme: light)" srcset="{image.url}">
-    <img alt="H3XOS" src="{image.url}">
+    <img alt="POPUGOS" src="{image.url}">
 </picture>
 
 <sub><i>Generated automatically using [x0rzavi/github-readme-terminal](https://github.com/x0rzavi/github-readme-terminal) on {time_now}</i></sub>
